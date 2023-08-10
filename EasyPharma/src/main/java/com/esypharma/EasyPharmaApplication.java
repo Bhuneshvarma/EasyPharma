52051635dbdd8c4dd58e71845ded1ba59092a57b
vishal gupta <guptavishal7676@gmail.com>
2023-08-10 14:03:29 +0530

    project setup

---
+++ EasyPharma/src/main/java/com/esypharma/EasyPharmaApplication.java
@@ -0,0 +1,13 @@
+package com.esypharma;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EasyPharmaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EasyPharmaApplication.class, args);
+	}
+
+}
